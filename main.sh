afe3597e90bd80c402051d6bb538765bcfe626ce
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2023-03-24 19:35:57 +0100

    Optimized Config Loading Pt. 2

--- main.sh
+++ main.sh
@@ -7,15 +7,13 @@ if [ ! -f $configfile ]; then
     touch $configfile
 fi
 
-. $configfile
+#. /$configfile
 
 counter=1
 while read -r line; do
     varname="line_$counter"
-    declare "$varname=$line"
+    
+    #checking every 30 seconds if sensitive data is present in logs
+    watch -n 30 logcat | grep $varname > /storage/emulated/0/ids.log && continue
     counter=$((counter+1))
 done < "$configfile"
-
-#checking every 30 seconds if sensitive data is present in logs
-watch -n 30 logcat | grep $line_1 > /storage/emulated/0/ids.log && watch -n 30 logcat | grep $line_2 > /storage/emulated/0/ids.log && watch -n 30 logcat | grep $line_3 > /storage/emulated/0/ids.log && continue
-
