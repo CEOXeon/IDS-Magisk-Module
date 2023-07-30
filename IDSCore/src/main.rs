15c785aa3e8a5c91e11b83bc7628a1adfabe7b02
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2023-07-30 20:24:33 +0200

    Implemented Simple CLI using Clap

--- IDSCore/src/main.rs
+++ IDSCore/src/main.rs
@@ -1,4 +1,39 @@
+use clap::Parser;
+
 #[allow(non_snake_case)]
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    clap();
+}
+
+
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn clap() {
+    let args = Args::parse();
+
+    if args.name == "Censored" || args.name == "CEOXeon" {
+        println!("Hi, Admin.");
+    } else if args.name == "MOD" || args.name == "Money" {
+        println!("Hi, the M's are meeting.");
+    } else {
+        println!("Normal User.");
+    }
+
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
