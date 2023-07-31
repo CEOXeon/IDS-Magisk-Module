6d93f41715b89e0d3d03d46295033b5fca54488c
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2023-07-31 20:54:17 +0200

    Added Simple Port Scanner

--- IDSCore/src/main.rs
+++ IDSCore/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::net::{TcpStream, ToSocketAddrs};
+use std::time::Duration;
 
 #[allow(non_snake_case)]
 fn main() {
@@ -28,6 +30,9 @@ fn clap() {
         println!("Hi, Admin.");
     } else if args.name == "MOD" || args.name == "Money" {
         println!("Hi, the M's are meeting.");
+    }
+      else if args.name == "port" {
+        port_scanner()
     } else {
         println!("Normal User.");
     }
@@ -35,5 +40,48 @@ fn clap() {
 
     for _ in 0..args.count {
         println!("Hello {}!", args.name)
+    }
+}
+
+fn is_port_open(host: &str, port: u16) -> bool {
+    match format!("{}:{}", host, port).to_socket_addrs() {
+        Ok(addresses) => {
+            for address in addresses {
+                if let Ok(_) = TcpStream::connect_timeout(&address, Duration::from_nanos(1)) {
+                    return true;
+                }
+            }
+            false
+        }
+        Err(_) => false,
+    }
+}
+
+fn scan_ports(host: &str, start_port: u16, end_port: u16) -> Vec<u16> {
+    let mut open_ports = Vec::new();
+
+    for port in start_port..=end_port {
+        if is_port_open(host, port) {
+            open_ports.push(port);
+        }
+    }
+
+    open_ports
+}
+
+fn port_scanner() {
+    let host = "127.0.0.1";
+    let start_port = 1;
+    let end_port = 16384;
+
+    let open_ports = scan_ports(host, start_port, end_port);
+
+    if open_ports.is_empty() {
+        println!("No open ports found.");
+    } else {
+        println!("Open ports:");
+        for port in open_ports {
+            println!("Port {} is open", port);
+        }
     }
 }
