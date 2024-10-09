c0702b63c36ffa5bb9d1479822e9bd49f3f6337f
CEOXeon <127042438+CEOXeon@users.noreply.github.com>
2024-10-09 14:33:27 +0200

    Added Simple Response Scheme

--- IDSCore/src/main.rs
+++ IDSCore/src/main.rs
@@ -1,9 +1,9 @@
 use std::net::SocketAddr;
 use clap::Parser;
 use async_std::task;
-use futures::future::join_all;
 use async_port_scanner::Scanner;
 use std::time::Duration;
+use std::process::Command;
 
 fn main() {
     // println!("Hello, world!");
@@ -43,5 +43,39 @@ fn idscore_scan_ports() {
 
     let ftr = ps.run_batched("127.0.0.1".to_string(), 1, 65535, 10000);
     let my_addrs: Vec<SocketAddr> = task::block_on(async { ftr.await });
-    println!("{:?}", my_addrs);
+
+    // Extract only the ports from the addresses
+    let mut ports: Vec<u16> = my_addrs.into_iter().map(|addr| addr.port()).collect();
+
+    println!("{:?}", ports);
+    ports.sort();
+
+    idscore_log(ports);
+
+}
+
+fn idscore_log(ports: Vec<u16>) {
+    for port in ports.iter() {
+        port.to_string();
+        if *port == 22 {
+            println!("SSH Port Open");
+            let cmd: String = r#"su -lp 2000 -c "cmd notification post -S bigtext -t 'IDS Magisk Module' 'SSH Port Alert' 'SSH Port is open /// Executing Emergency Response'""#.to_string();
+            Command::new("sh")
+                .arg("-c")
+                .arg(cmd)
+                .output()
+                .expect("failed to execute process");
+        } else if *port == 80 {
+            println!("HTTP Port Open");
+            Command::new("sh")
+                .arg("-c")
+                .arg("echo 'HTTP Port Open' >> /var/log/idscore.log")
+                .output()
+                .expect("failed to execute process");
+        } else if *port == 443 {
+            println!("HTTPS Port Open");
+        } else {
+            println!("Port {} is Open", port);
+        }
+    }
 }
